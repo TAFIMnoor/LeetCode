5c4baa6149490ca13dfb9c5ffd5c793d3e87468c
TAFIMnoor <43815369+TAFIMnoor@users.noreply.github.com>
2022-03-25 01:42:13 +0600

    ALGO Study Plan

---
+++ Binary Search.cpp
@@ -0,0 +1,46 @@
+// ** Approach : Recursion **
+
+class Solution {
+public:
+    int binarySearch(vector<int>& nums, int l, int r, int t){
+        if( r >= l ) {
+            
+            int m = l + (r-l)/2;
+        
+            if( nums[m] == t ) 
+                return m;
+            if( nums[m] > t )
+                return binarySearch(nums, l, r-1, t);
+            if( nums[m] < t )
+                 return binarySearch(nums, l+1, r, t);
+            
+        }
+        
+        return -1;
+    }
+    int search(vector<int>& nums, int target) {
+        int ans = binarySearch(nums, 0, nums.size() -1, target);
+        return ans;
+    }
+};
+
+// ** Approach : Loop **
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size()-1;
+        
+        while( l <= r ){
+            int m = l + (r-l)/2;
+            if(nums[m] == target)
+                return m;
+            if( nums[m] > target )
+                r--;
+            if( nums[m] < target )
+                l++;
+        }
+        
+        return -1;
+    }
+};
